1d4510535e3b2c378e38b2393d1f967ec1d3092e
swagm0nster <98782228+swagm0nster@users.noreply.github.com>
2023-08-21 16:39:15 -0400

    changed margin

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
 export default function Pagination({ goToNextPage, goToPrevPage }) {
-    
     return (
-    <div className='flex flex-row justify-center mb-14'>
+    <div className='flex flex-row justify-center'>
         {goToPrevPage && <button onClick={goToPrevPage} className='mr-4 py-2 px-4 rounded-md bg-white border-2 border-black hover:bg-red-500 hover:text-white hover:cursor-pointer'>Previous</button>}
         {goToNextPage && <button onClick={goToNextPage} className='py-2 px-4 rounded-md bg-white border-2 border-black hover:bg-red-500 hover:text-white hover:cursor-pointer'>Next</button>}
     </div>
