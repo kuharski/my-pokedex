5a626cecee57dd92606b53e1edb458383cd7972e
swagm0nster <98782228+swagm0nster@users.noreply.github.com>
2023-08-21 16:50:11 -0400

    added font

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Pagination({ goToNextPage, goToPrevPage }) {
     return (
     <div className='flex flex-row justify-center'>
-        {goToPrevPage && <button onClick={goToPrevPage} className='mr-4 py-2 px-4 rounded-md bg-white border-2 border-black hover:bg-red-500 hover:text-white hover:cursor-pointer'>Previous</button>}
-        {goToNextPage && <button onClick={goToNextPage} className='py-2 px-4 rounded-md bg-white border-2 border-black hover:bg-red-500 hover:text-white hover:cursor-pointer'>Next</button>}
+        {goToPrevPage && <button onClick={goToPrevPage} className='font-["Tektur"] font-semibold mr-4 py-2 px-4 rounded-md bg-white border-2 border-black hover:bg-red-500 hover:text-white hover:cursor-pointer'>Previous Page</button>}
+        {goToNextPage && <button onClick={goToNextPage} className='font-["Tektur"] font-semibold py-2 px-4 rounded-md bg-white border-2 border-black hover:bg-red-500 hover:text-white hover:cursor-pointer'>Next Page</button>}
     </div>
   );
 }
