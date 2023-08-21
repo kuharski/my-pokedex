cd32ee933af20d9596a3ec49a8320ef0ff2ad6f8
swagm0nster <98782228+swagm0nster@users.noreply.github.com>
2023-08-21 12:27:37 -0400

    next prev buttons

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function Pagination() {
-  return (
-    <div>
-        
+export default function Pagination({ goToNextPage, goToPrevPage }) {
+    
+    return (
+    <div className='flex flex-row justify-center mb-14'>
+        {goToPrevPage && <button onClick={goToPrevPage} className='mr-4 py-2 px-4 rounded-md bg-white border-2 border-black hover:bg-red-500 hover:text-white hover:cursor-pointer'>Previous</button>}
+        {goToNextPage && <button onClick={goToNextPage} className='py-2 px-4 rounded-md bg-white border-2 border-black hover:bg-red-500 hover:text-white hover:cursor-pointer'>Next</button>}
     </div>
-  )
+  );
 }
