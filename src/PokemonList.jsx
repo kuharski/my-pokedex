cbfd04d5b24405f4dfd51a49c26331e4efbc304f
swagm0nster <98782228+swagm0nster@users.noreply.github.com>
2023-08-21 12:30:23 -0400

    loading and pokemonlist

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 
-export default function PokemonList({ pokemon }) {
-  return (
-    <div>
-        {pokemon.map((p) => (
-            <div key={p}>
-                {p}
+
+export default function PokemonList({ pokemon, loading }) {
+    
+    if(loading) {
+        return (
+            <div className='flex flex-col justify-center align-middle h-[43.5rem] md:h-[48.5rem] w-full'>
+                <div className="flex justify-center">
+                    <img alt="loading" src={process.env.PUBLIC_URL + '/pokeball.png'} className="w-16 h-16 animate-spin" />
+                </div>
             </div>
-        ))}
-    </div>
-  )
+        );
+    } else {
+        return (
+            <div className='flex justify-center mt-10 mb-4'>
+                <ul>
+                    {pokemon.map((name) => (
+                        <li key={name} className='text-base md:text-lg lg:text-xl text-center py-1'>
+                            {name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
 }
